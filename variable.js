9c8fd900603d3adf355b9d361995e66970db0f76
jioun.lee.dev <jioun.lee.dev@gmail.com>
2020-07-28 23:13:16 +0900

    #3 variables 강의 보충

--- variable.js
+++ variable.js
@@ -3,7 +3,7 @@
 // added ECMAScript 5
 'use strict';
 
-// 2. Variable
+// 2. Variable, rw(read/ write)
 // let (added in ES6)
 let name = 'elle';
 console.log(name);
@@ -19,17 +19,23 @@ console.log(name);
 }
 console.log(age);
 
-// 3. Contants
+// 3. Constant, r(read only)
+// use const whenever posiible.
+// only use let if variable needs to change.
+const daysInWeek = 7;
+const maxNumber = 5;
+
+// Note!
+// Immutable data types: premitive types, frozen objects (i.e. object.freeze())
+// Mutable data types: all objects by default are mutable in JS
 // favor immutable data type always for a few reasons:
 // - SecurityPolicyViolationEvent
 // - thread safety
 // -reduce human mistakes
-const daysInWeek = 7;
-const maxNumber = 5;
 
 // 4. Variable types
 // primitive single item: number, string. boolean, null, undefined, symbol
-// object, box container (single item 여러 개의 모음)
+// object, box container (single item 여러 개의 모음, 용량이 커서 메모리에 한꺼번에 올라갈 수 없음)
 // function, first-class function 
 const count = 17; // integer
 const size = 17.1; // decimal number
