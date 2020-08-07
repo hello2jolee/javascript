f4c3f6a27216f68eca69f13f4df2ba0fbd5cfd29
jioun.lee.dev <jioun.lee@nts-corp.com>
2020-08-07 22:31:21 +0900

    #16 자바스크립트 수정 중

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,42 @@
 'use strict';
 
-console.log('Hello World')
+const stocks = [
+    {color: 'pink', clothType: 'tee', gender: 'female', clothSize: 'large'},
+    {color: 'blue', clothType: 'pants', gender: 'man', clothSize: 'small'},
+    {color: 'yellow', clothType: 'pants', gender: 'man', clothSize: 'large'},
+    {color: 'yellow', clothType: 'skirt', gender: 'man', clothSize: 'large'},
+    {color: 'blue', clothType: 'skirt', gender: 'female', clothSize: 'small'},
+    {color: 'blue', clothType: 'tee', gender: 'male', clothSize: 'large'},
+    {color: 'yellow', clothType: 'tee', gender: 'male', clothSize: 'large'},
+    {color: 'pink', clothType: 'pants', gender: 'female', clothSize: 'small'},
+    {color: 'pink', clothType: 'tee', gender: 'female', clothSize: 'large'},
+    {color: 'blue', clothType: 'pants', gender: 'male', clothSize: 'small'},
+    {color: 'yellow', clothType: 'pants', gender: 'male', clothSize: 'large'},
+    {color: 'yellow', clothType: 'skirt', gender: 'man', clothSize: 'large'},
+    {color: 'blue', clothType: 'skirt', gender: 'female', clothSize: 'small'},
+    {color: 'blue', clothType: 'tee', gender: 'male', clothSize: 'large'}
+]
+
+function selectedColor(color) {
+    console.log(color)
+    const result = ''
+    if(color !== null) {
+        result = stocks.filter((stock) => stock.color)
+    } 
+    console.log(result)
+    const list = document.querySelector("#clothesList")
+    if(color === 'pink') {
+        list.innerHTML += "<li class='item'><img src='img/pink_p.png' alt='pink pants' width='20' height='20'/><p>"+{}+"</p></li>"
+    }
+}
+
+function selectedclothType(clothType) {
+    if(clothType !== null) {
+        const result = stocks.filter((stock) => stock.clothType)
+    } 
+    const list = document.querySelector("#clothesList")
+    if(clothType === 'tee') {
+        list.innerHTML += "<li class='item'><img src='img/pink_p.png' alt='pink pants' width='20' height='20'/><p>"+{}+"</p></li>"
+    }
+}
+selectedColor('pink')
