274335b17aeecf1135fe0d2808acf48078ae957e
jioun.lee.dev <jioun.lee@nts-corp.com>
2020-08-08 23:33:31 +0900

    #16 javascript 수정 중

--- src/js/main.js
+++ src/js/main.js
@@ -1,33 +1,43 @@
 'use strict';
 
+class Stock {
+    constructor(color, clothType, gender, clothSize) {
+        this.color = color
+        this.clothType = clothType
+        this.gender = gender
+        this.clothSize = clothSize
+    }
+}
 const stocks = [
-    {color: 'pink', clothType: 'tee', gender: 'female', clothSize: 'large'},
-    {color: 'blue', clothType: 'pants', gender: 'man', clothSize: 'small'},
-    {color: 'yellow', clothType: 'pants', gender: 'man', clothSize: 'large'},
-    {color: 'yellow', clothType: 'skirt', gender: 'man', clothSize: 'large'},
-    {color: 'blue', clothType: 'skirt', gender: 'female', clothSize: 'small'},
-    {color: 'blue', clothType: 'tee', gender: 'male', clothSize: 'large'},
-    {color: 'yellow', clothType: 'tee', gender: 'male', clothSize: 'large'},
-    {color: 'pink', clothType: 'pants', gender: 'female', clothSize: 'small'},
-    {color: 'pink', clothType: 'tee', gender: 'female', clothSize: 'large'},
-    {color: 'blue', clothType: 'pants', gender: 'male', clothSize: 'small'},
-    {color: 'yellow', clothType: 'pants', gender: 'male', clothSize: 'large'},
-    {color: 'yellow', clothType: 'skirt', gender: 'man', clothSize: 'large'},
-    {color: 'blue', clothType: 'skirt', gender: 'female', clothSize: 'small'},
-    {color: 'blue', clothType: 'tee', gender: 'male', clothSize: 'large'}
+    new Stock('pink', 'tee', 'female', 'large'),
+    new Stock('blue', 'pants', 'man', 'small'),
+    new Stock('yellow', 'pants', 'man', 'large'),
+    new Stock('yellow', 'skirt', 'man', 'large'),
+    new Stock('blue', 'skirt', 'female', 'small'),
+    new Stock('blue', 'tee', 'male', 'large'),
+    new Stock('yellow', 'tee', 'male', 'large'),
+    new Stock('pink', 'pants', 'female', 'small'),
+    new Stock('pink', 'tee', 'female', 'large'),
+    new Stock('blue', 'pants', 'male', 'small'),
+    new Stock('yellow', 'pants', 'male', 'large'),
+    new Stock('yellow', 'skirt', 'man', 'large'),
+    new Stock('blue', 'skirt', 'female', 'small'),
+    new Stock('blue', 'tee', 'male', 'large')
 ]
 
-function selectedColor(color) {
-    console.log(color)
-    const result = ''
-    if(color !== null) {
-        result = stocks.filter((stock) => stock.color)
-    } 
+function selectedColor(selectColor) {
+    console.log(selectColor)
+    
+    const result = stocks
+        .map((stock, index) => `${stock.color}, ${index}`)
+        // .filter(color => color === selectColor)
     console.log(result)
-    const list = document.querySelector("#clothesList")
-    if(color === 'pink') {
-        list.innerHTML += "<li class='item'><img src='img/pink_p.png' alt='pink pants' width='20' height='20'/><p>"+{}+"</p></li>"
-    }
+
+    // console.log(result)
+    // const list = document.querySelector("#clothesList")
+    // if(color === 'pink') {
+    //     list.innerHTML += "<li class='item'><img src='img/pink_p.png' alt='pink pants' width='20' height='20'/><p>"+{}+"</p></li>"
+    // }
 }
 
 function selectedclothType(clothType) {
