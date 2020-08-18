77123b20920708e1a2929617d9ba3c88be975bcd
jioun.lee.dev <jioun.lee@nts-corp.com>
2020-08-18 21:35:56 +0900

    MongoDB Model & Schema

--- boiler-plate/index.js
+++ boiler-plate/index.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://jolee:<pwd>@boilerplate.psdje.mongodb.net/<dbname>?retryWrites=true&w=majority', {
+mongoose.connect('mongodb+srv://jolee:<pwd>@boilerplate.psdje.mongodb.net/test?retryWrites=true&w=majority', {
     useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true, useFindAndModify: false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
